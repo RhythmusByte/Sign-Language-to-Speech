2bd92df74a8ad2d82dc0b6159cf8dfaeea27c7d0
sanoojk820@gmail.com <akhilmahesh@proton.me>
2025-02-06 12:57:24 +0530

    Added Rectangle Box

--- dataCollection.py
+++ dataCollection.py
@@ -2,10 +2,17 @@ import cv2
 from cvzone.HandTrackingModule import HandDetector
 
 cap = cv2.VideoCapture(0)
-detector = HandDetector(maxHands=1) # Maximum Hands Detected
+detector = HandDetector(maxHands=8) # Maximum Hands Detected
 
 while True:
     success, img = cap.read()
     hands, img = detector.findHands(img)
+
+    if hands:
+        hand = hands[0]
+        x, y, w, h = hand['bbox']
+        imgCrop = img[y : y + h, x : x + w]
+        cv2.imshow("ImageCrop", imgCrop)
+
     cv2.imshow("Image", img)
     cv2.waitKey(1)
