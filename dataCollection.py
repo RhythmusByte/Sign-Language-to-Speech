d2ade60e853a0ccbdbf595af25e30f36684349d3
sanoojk820@gmail.com <akhilmahesh@proton.me>
2025-02-07 11:22:33 +0530

    Added aspect ratio handling for hand crop resizing to maintain proportions.

--- dataCollection.py
+++ dataCollection.py
@@ -1,10 +1,12 @@
 import cv2
-from PIL.ImageChops import offset
 from cvzone.HandTrackingModule import HandDetector
+import numpy as np
+import math
 
 cap = cv2.VideoCapture(0)
-detector = HandDetector(maxHands=2)
+detector = HandDetector(maxHands=1)
 offset = 20
+imgSize = 300
 
 while True:
     success, img = cap.read()
@@ -13,8 +15,21 @@ while True:
     if hands:
         hand = hands[0]
         x, y, w, h = hand['bbox']
-        imgCrop = img[y - offset : y + h + offset, x : x + w]
+        imgWhite = np.ones((imgSize, imgSize, 3), np.uint8) * 255
+
+        imgCrop = img[y - offset : y + h + offset, x - offset : x + w + offset]
+
+        aspectRatio = h / w
+
+        if aspectRatio > 1:
+            k = imgSize / h
+            wCal = math.ceil(k * w)
+            imgResize = cv2.resize(imgCrop, (wCal, imgSize))
+            imgResizeShape = imgResize.shape
+        imgWhite[0:imgResizeShape[0],0:imgResizeShape[1]] = imgResize
+
         cv2.imshow("ImageCrop", imgCrop)
+        cv2.imshow("ImageWhite", imgWhite)
 
     cv2.imshow("Image", img)
     cv2.waitKey(1)
